216e4243d8408fd6d157fd10afabbb17f6dcd4b3
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-12-28 20:20:48 +0800

    init

---
+++ database.php
@@ -0,0 +1,8 @@
+<?php 
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "lihimnaliham";
+
+$connect = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
+?>
