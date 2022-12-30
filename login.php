ffbea6ea25ef3a226b28f4b9656a79e662fda992
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-12-30 18:21:46 +0800

    login ui

--- login.php
+++ login.php
@@ -35,7 +35,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Sacramento&display=swap" rel="stylesheet">
     <title>Login</title>
 </head>
-<body>
+<body class="hidden">
+        <a href="index.html" class="home">Home</a>
         <div class="login-form-container">
             <h3>Lihim na Liham</h3>
             <form method="post">
