eda70eed1edd89cb52c82187bece449635464b0d
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-12-30 17:12:13 +0800

    init

--- login.php
+++ login.php
@@ -1,3 +1,30 @@
+<?php 
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    include("database.php");
+
+    $sql = sprintf("SELECT * FROM admin
+                    WHERE username = '%s'",
+                   $connect->real_escape_string($_POST["email"]));
+
+    $result = $connect->query($sql);
+    $user = $result->fetch_assoc();
+
+    if($user){
+        if (password_verify($_POST["password"], $user["password"])) 
+        {
+            session_start();
+            $_SESSION["user_id"] = $user["id"];
+            header("Location: lnladmin.php");
+            exit;
+        }
+    }
+    $is_invalid = true;
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +34,20 @@
     <title>Login</title>
 </head>
 <body>
-    
+<div class="login-form-container">
+            <?php if ($is_invalid): ?>
+                <em>Wrong email or password!</em>
+            <?php endif; ?>
+
+            <form method="post">
+                    <input placeholder="Username" type="text" name="email" id="email"
+                    value="<?= htmlspecialchars($_POST["email"] ?? "") ?>">
+                
+                    <input placeholder="Password" type="password" name="password" id="password">
+
+                    <button>Login</button>
+            </form>
+        </div>
+
 </body>
 </html>
