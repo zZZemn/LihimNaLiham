1bde0a59fc91dd5efcc81d249fec4e9b96784c54
Emmanuel Ugaban <ugabane0516@gmail.com>
2023-01-23 20:35:25 +0800

    walang kwentang api remove

--- lnladmin.php
+++ lnladmin.php
@@ -11,27 +11,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&family=Sacramento&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Lihim na Liham</title>
 </head>
 <body>
-        <ul class="flex-column bg-dark p-5 " style="width: 10%;">
-            <li class="nav-item">
-               <a href="#">Sample</a> 
-            </li>
-            <li class="nav-item">
-               <a href="#">Sample</a> 
-            </li>
-            <li class="nav-item">
-               <a href="#">Sample</a> 
-            </li>
-            <li class="nav-item">   
-               <a href="#">Sample </a> 
-            </li>
-        </ul>
+       
 
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </body>
 </html>
